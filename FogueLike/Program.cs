de48ef5f19a9b5f18d9e1bf87579e5926cbf16e0
falldj <derek.isensee@gmail.com>
2017-03-27 22:32:40 +0000

    Initial commit

---
+++ FogueLike/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace FogueLike
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Hey!");
+        }
+    }
+}
